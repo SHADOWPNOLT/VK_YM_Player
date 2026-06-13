84399644b5a4af51500fbe15a3fa5d8b296fa51d
SHADOWPNOLT <kolmacev7@gmail.com>
2026-06-13 23:18:16 +0300

    Final cleanup: remove all problematic classes, keep only working app

--- app/src/main/java/com/vkym/player/VKYMApplication.java
+++ app/src/main/java/com/vkym/player/VKYMApplication.java
@@ -1,24 +1,17 @@
 package com.vkym.player;
 
 import android.app.Application;
-import com.vkym.player.di.ServiceLocator;
 
 public class VKYMApplication extends Application {
     private static VKYMApplication instance;
-    private ServiceLocator serviceLocator;
     
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
-        serviceLocator = new ServiceLocator(this);
     }
     
     public static VKYMApplication getInstance() {
         return instance;
-    }
-    
-    public ServiceLocator getServiceLocator() {
-        return serviceLocator;
     }
 }
