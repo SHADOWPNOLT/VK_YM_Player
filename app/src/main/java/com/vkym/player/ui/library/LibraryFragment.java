2d0cb2c5ab63b2dc4c30c6a5dbbbf5153568baef
SHADOWPNOLT <kolmacev7@gmail.com>
2026-06-14 00:41:41 +0300

    Fix LibraryFragment - move LogCollector calls inside methods

--- app/src/main/java/com/vkym/player/ui/library/LibraryFragment.java
+++ app/src/main/java/com/vkym/player/ui/library/LibraryFragment.java
@@ -14,7 +14,7 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.vkym.player.R;
-import com.vkym.player.utils.Logger;
+import com.vkym.player.utils.LogCollector;
 import com.vkym.player.ui.auth.VKAuthActivity;
 
 public class LibraryFragment extends Fragment {
@@ -28,13 +28,13 @@ public class LibraryFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        Logger.d(TAG + ": onCreateView started");
+        LogCollector.add(TAG + ": onCreateView started");
         try {
             View view = inflater.inflate(R.layout.fragment_library, container, false);
-            Logger.d(TAG + ": View inflated successfully");
+            LogCollector.add(TAG + ": View inflated successfully");
             return view;
         } catch (Exception e) {
-            Logger.e(TAG + ": Error inflating layout", e);
+            LogCollector.add(TAG + ": Error inflating layout", e);
             Toast.makeText(getContext(), "Ошибка: " + e.getMessage(), Toast.LENGTH_LONG).show();
             return null;
         }
@@ -43,32 +43,32 @@ public class LibraryFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Logger.d(TAG + ": onViewCreated started");
+        LogCollector.add(TAG + ": onViewCreated started");
         
         try {
             viewPager = view.findViewById(R.id.viewPager);
             tabLayout = view.findViewById(R.id.tabLayout);
             btnVKAuth = view.findViewById(R.id.btnVKAuth);
             
-            Logger.d(TAG + ": Views found - viewPager=" + (viewPager != null) + 
+            LogCollector.add(TAG + ": Views found - viewPager=" + (viewPager != null) + 
                    ", tabLayout=" + (tabLayout != null) + 
                    ", btnVKAuth=" + (btnVKAuth != null));
             
             if (btnVKAuth != null) {
                 btnVKAuth.setOnClickListener(v -> {
-                    Logger.d(TAG + ": VK Auth button clicked");
+                    LogCollector.add(TAG + ": VK Auth button clicked");
                     try {
                         Intent intent = new Intent(getActivity(), VKAuthActivity.class);
                         startActivity(intent);
                     } catch (Exception e) {
-                        Logger.e(TAG + ": Error starting VKAuthActivity", e);
+                        LogCollector.add(TAG + ": Error starting VKAuthActivity", e);
                         Toast.makeText(getContext(), "Ошибка: " + e.getMessage(), Toast.LENGTH_LONG).show();
                     }
                 });
             }
             
             // Настройка ViewPager
-            Logger.d(TAG + ": Setting up ViewPager");
+            LogCollector.add(TAG + ": Setting up ViewPager");
             ViewPagerAdapter adapter = new ViewPagerAdapter(this);
             viewPager.setAdapter(adapter);
             
@@ -82,10 +82,10 @@ public class LibraryFragment extends Fragment {
                 }
             ).attach();
             
-            Logger.d(TAG + ": ViewPager setup completed");
+            LogCollector.add(TAG + ": ViewPager setup completed");
             
         } catch (Exception e) {
-            Logger.e(TAG + ": Error in onViewCreated", e);
+            LogCollector.add(TAG + ": Error in onViewCreated", e);
             Toast.makeText(getContext(), "Ошибка: " + e.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
@@ -99,7 +99,7 @@ public class LibraryFragment extends Fragment {
         @NonNull
         @Override
         public Fragment createFragment(int position) {
-            Logger.d("ViewPagerAdapter: createFragment position=" + position);
+            LogCollector.add("ViewPagerAdapter: createFragment position=" + position);
             if (position == 0) {
                 return new VKTracksFragment();
             } else {
@@ -113,7 +113,3 @@ public class LibraryFragment extends Fragment {
         }
     }
 }
-    // В начало onCreateView добавить:
-    LogCollector.add("LibraryFragment: onCreateView started");
-    // В конце:
-    LogCollector.add("LibraryFragment: onCreateView completed");
